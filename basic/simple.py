854375e362ad9bdda5c667b0fb4c585d64607028
Rakhavendra S M <59560301+Rakhavendra99@users.noreply.github.com>
2020-09-07 22:51:24 +0530

    Create simple.py

---
+++ basic/simple.py
@@ -0,0 +1,46 @@
+#program 2
+#simple addition
+tamil,english,maths=90,85,100
+tamil+english,maths
+#output 273
+
+
+#program3
+#basic calculation
+salary = 50000
+tax = salary*(10/100)
+tax
+#output 5000.0
+#same program to using int fuction
+salary = 50000
+tax = salary*(10/100)
+int(tax)
+#output 5000
+
+#program 4
+#what purpose of using underscore(_)?
+#last to print value to be stored in the underscore function
+tamil = 100
+tamil
+#output 100
+tamil + _
+#output 200
+tamil + _
+#output 300
+tax = 50000
+tax
+#output 50000
+tamil + _ 	#this place to added in last printed value,it's automaticaly saved.
+#output 50100
+
+
+#progam 5
+price = 121.99
+tax = 4.5
+price*tax
+#output 548.95499999999999
+round(_,2)	# this place is to last stored value to be rounded and only 2 digit value to be show this function
+#output 548.95
+round(_)	#last sotred value to be rounded function
+#output 549
+
